737acd303741b1736c15721671386c9face36955
Jaime van Kessel <nallath@gmail.com>
2015-12-14 10:01:49 +0100

    Removed unused code
    EM-65

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,23 +10,6 @@
 int main(int argc, char** argv)
 {
     Clock elapsed_time;
-    //zxing::Ref<zxing::LuminanceSource> source;
-    /*CurlRequest test("http://10.180.1.150:8080/?action=snapshot");
-
-
-    int width, height;
-    int size = 800 * 600 * 3;
-    int comps = 0;
-    zxing::ArrayRef<char> image;
-    char *buffer = reinterpret_cast<char*>(jpgd::decompress_jpeg_image_from_memory(reinterpret_cast<const unsigned char *>(test.getData().c_str()), size, &width, &height, &comps, 4));
-    image = zxing::ArrayRef<char>(buffer, 4 * width * height);
-    free(buffer);
-    zxing::Ref<zxing::LuminanceSource> source = zxing::Ref<zxing::LuminanceSource>(new ImageReaderSource(image, width, height, comps));
-
-    int result = read_image(source, true);
-
-
-    std::cout << result << std::endl;*/
     QRDetector detector("http://10.180.1.150:8080/?action=snapshot");
 
     while(true)
